0908c29291413472980f23b94934e11ea0fb4faa
Sangmin Kim <smgeem@korea.ac.kr>
2021-08-11 21:36:27 +0900

    210811 - Monte Carlo

--- main.m
+++ main.m
@@ -1,105 +1,67 @@
 function [] = main ()
 
-% Parameters
-K = 128;                    % # of subcarriers
-Rcom = 6;                   % channel length
-Rrad = 64;                  % # of range cells
-sigma = sqrt(1e-3);         % std.          (input SNR = 10^3)
+iterations = cell(100, 1);
+Zplot = cell(100, 1);
+SNRplot = cell(100, 1);
+ERRORplot = cell(100, 1);
+SS = cell(100, 1);
+iterations_len = zeros(100, 1);
 
-% Communication Channel
-hdB = zeros(K, 1);
-hdB(1:Rcom, 1) = [-6.0 0.0 -7.0 -22.0 -16.0 -20.0];    % power profile
-h = zeros(K, 1);
-h(1:Rcom, 1) = 10.^(hdB(1:Rcom, 1)/10);
-H = fft(h);                                            % frequency response
-
-% Threshold
-SNRmin = 10.^(-30/10);                      % threshold SNR : -30dB
-rho = sigma * sqrt(SNRmin) ./ abs(H);       % threshold power of each subcarrier rho = [rho(0) rho(1) ... rho(K-1)]
-
-% Initialization
-d = randn(K, 1);
-d = d / norm(d);                % OFDM freq domain d = [d(0) d(1) ... d(K-1)]
-
-Rand = orth(randn(K, K));
-Q = Rand(:, 1:Rrad);            % semiunitary matrix
-s = K * ifft(d);                % OFDM time domain s = [s(0) s(1) ... s(K-1)]
-S = zeros(K, Rrad);
-for idx = 1 : Rrad
-    S(:,idx) = circshift(fliplr(s), K-Rrad+idx);
+for idx = 1 : 100
+    [iterations{idx}, Zplot{idx}, SNRplot{idx}, ERRORplot{idx}, SS{idx}] = cyclic ();
+    iterations_len(idx, 1) = length(iterations{idx});
 end
 
-% Objective Function
-y = norm(S'*S - Q'*Q, 'fro');
+min_len = min(iterations_len);
+it = zeros(1, min_len);
 
-jdx = 0;
-z = y;
-
-iterations = zeros(0, 0);
-Zplot = zeros(0, 0);
-SNRplot = zeros(0, 0);
-ERRORplot = zeros(0, 0);
-
-while true
-    y = z;
-    % #1 : Obtain S given Q
-    S = alg1 (K, Rrad, rho, Q);
-    
-    % #2 : Obtain Q given S
-    [U, ~, V] = svd(S');
-    Vtilde = V(:, 1:Rrad);
-    Q = sqrt(K) * Vtilde * U';
-    
-    z = norm(S'*S - Q'*Q, 'fro');
-    
-    SS = S' * S;
-    
-    SNRrad = sum(1 ./ diag(inv(SS))) / Rrad;
-    SNRraddB = 10 * log(SNRrad) / log(10);
-    
-    s = S(:,Rrad);
-    d = (1/K) * fft(s);
-    
-    SNRcom = abs(d).^2 .* abs(H).^2 / sigma^2;
-    ErrorP = sum(erfc(SNRcom ./ sqrt(2))) ./ K;
-    
-    jdx = jdx + 1;
-    iterations = horzcat(iterations, jdx);
-    Zplot = horzcat(Zplot, z);
-    SNRplot = horzcat(SNRplot, SNRraddB);
-    ERRORplot = horzcat(ERRORplot, ErrorP);
-    
-    if abs(z-y) < 1e-5      % stopping criterion
-        break
-    end
-    
+for jdx = 1 : min_len
+    it(jdx) = jdx;
 end
+
+Zpl = zeros(100, min_len);
+SNRpl = zeros(100, min_len);
+ERRORpl = zeros(100, min_len);
+
+for kdx = 1 : 100
+    ZZpl = Zplot{kdx, 1};
+    SSNRpl = SNRplot{kdx, 1};
+    EERRORpl = ERRORplot{kdx, 1};
+    
+    Zpl(kdx, :) = ZZpl(1, 1:min_len);
+    SNRpl(kdx, :) = SSNRpl(1, 1:min_len);
+    ERRORpl(kdx, :) = EERRORpl(1, 1:min_len);
+
+end
+
+Zmean = mean(Zpl);
+SNRmean = mean(SNRpl);
+ERRORmean = mean(ERRORpl);
 
 figure
-plot(iterations, Zplot, 'LineWidth', 1.5);
+plot(it, Zmean, 'LineWidth', 1.5);
 xlabel('# of iterations');
 ylabel('Objective Function');
 title('Objective Function Convergence');
 grid on
 
 figure
-plot(iterations, SNRplot, 'LineWidth', 1.5);
+plot(it, SNRmean, 'LineWidth', 1.5);
 xlabel('# of iterations');
 ylabel('Radar SNR (dB)');
 title('Radar SNR Convergence');
 grid on
 
 figure
-plot(iterations, ERRORplot, 'LineWidth', 1.5);
+plot(it, ERRORmean, 'LineWidth', 1.5);
 xlabel('# of iterations');
 ylabel('Error Probability');
 title('Error Probability Convergence');
 grid on
 
 figure
-SS = S' * S;
-[xx, yy] = meshgrid(1:Rrad, 1:Rrad);
-plot3(xx, yy, abs(SS), 'LineWidth', 1.5);
+[xx, yy] = meshgrid(1:64, 1:64);
+plot3(xx, yy, abs(SS{1}), 'LineWidth', 1.5);
 zlabel('|{S^H}S|')
 title('Diagonality of {S^H}S');
 grid on
